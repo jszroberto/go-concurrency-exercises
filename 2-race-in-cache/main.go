0d8049fbd9c87bb72635ab93dfdecf1650ea1310
Roberto Jimenez Sanchez <jszroberto@gmail.com>
2019-02-20 18:56:11 +0100

    Resolve race-in-cache

--- 2-race-in-cache/main.go
+++ 2-race-in-cache/main.go
@@ -8,7 +8,10 @@
 
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"sync"
+)
 
 // CacheSize determines how big the cache can grow
 const CacheSize = 100
@@ -24,6 +27,7 @@ type KeyStoreCache struct {
 	cache map[string]string
 	pages list.List
 	load  func(string) string
+	mtx   sync.Mutex
 }
 
 // New creates a new KeyStoreCache
@@ -36,6 +40,9 @@ func New(load KeyStoreCacheLoader) *KeyStoreCache {
 
 // Get gets the key from cache, loads it from the source if needed
 func (k *KeyStoreCache) Get(key string) string {
+	k.mtx.Lock()
+	defer k.mtx.Unlock()
+
 	val, ok := k.cache[key]
 
 	// Miss - load from database and save it in cache
